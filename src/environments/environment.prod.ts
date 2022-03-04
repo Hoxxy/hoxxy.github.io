97c1abcf61ee3facef11d95ef146a2889b91d1f3
Hoxxy <nikola.karailic@gmail.com>
2022-03-04 15:27:13 +0100

    Initial app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+    apiKey: "AIzaSyC3dZnr3KqhfchWckoy4VNB_khM7UAMa4c",
+    authDomain: "ecommerce-5baba.firebaseapp.com",
+    projectId: "ecommerce-5baba",
+    storageBucket: "ecommerce-5baba.appspot.com",
+    messagingSenderId: "516482334313",
+    appId: "1:516482334313:web:8ed6ccb8942a29a4faacf8",
+    measurementId: "G-4QKV198YNV",
+    production: false
+  }
 };
